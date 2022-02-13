6e02ff13cf101fa6966589c208d1edbb90996a9a
Guilherme Luiz <guilhermelpp.eng@gmail.com>
2022-02-13 12:18:53 -0300

    Teste de interoperatibilidade

--- src/Algebra/MatrizBase.php
+++ src/Algebra/MatrizBase.php
@@ -16,7 +16,6 @@ abstract class MatrizBase implements \ArrayAccess, \JsonSerializable
     public function obtenhaMatriz(): array
     {
         $retorno = [];
-        var_dump(1 == $this->NumeroLinha);
         if (1 == $this->NumeroLinha) {
             return array_map(fn (float|int $n) => normalizar($n), $this->matriz[0]);
         }
